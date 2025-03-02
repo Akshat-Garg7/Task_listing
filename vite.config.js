0b0b8ffcfb102a9e67772ba71f694f54f9a8504f
Akshat-Garg7 <akshatgarg280503@gmail.com>
2025-03-02 12:11:04 +0530

    test

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/Task_listing/', // Replace with your repository name
+  base: '/', // Replace with your repository name
 });
