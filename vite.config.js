590cfee648fad796af50d157e821384ef4272cc7
Akshat-Garg7 <akshatgarg280503@gmail.com>
2025-03-02 12:01:36 +0530

    Configured GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: 'https://github.com/Akshat-Garg7/Task_listing', // Replace with your repository name
+  base: '/Task_listing/', // Replace with your repository name
 });
